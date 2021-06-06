6583b442c39237581c263a8970f574a13d523fc1
GersonST <gersonsevero90@gmail.com>
2021-06-06 00:35:21 -0300

    Lógica finalizada

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# to-do-list
+Projeto de uma To-do-list feita com React
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import TodoList from './components/TodoList'
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Lista de Tarefas</h1>
+        <TodoList/>
+      </div>
+    );
+  }
+}
+
+export default App;
